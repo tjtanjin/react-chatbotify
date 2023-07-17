e08215a6c994b9181a94de9b338ad2ca54bef800
tjtanjin <cjtanjin@gmail.com>
2023-07-17 22:57:05 +0800

    refactor: Cleanup export types

--- src/export.tsx
+++ src/export.tsx
@@ -2,6 +2,11 @@ import ChatBot from "./components/ChatBot";
 import { useBotOptions, BotOptionsContext } from "./context/BotOptionsContext";
 import { useMessages, MessagesContext } from "./context/MessagesContext";
 import { usePaths, PathsContext } from "./context/PathsContext";
+import { Block } from "./types/Block";
+import { Flow } from "./types/Flow";
+import { Message } from "./types/Message";
+import { Options } from "./types/Options";
+import { Params } from "./types/Params";
 
 /**
  * Exports for developer use.
@@ -14,6 +19,14 @@ export {
 	useMessages,
 	PathsContext,
 	usePaths
-}
+};
+
+export type {
+	Block,
+	Flow,
+	Message,
+	Options,
+	Params
+};
 
 export default ChatBot;
