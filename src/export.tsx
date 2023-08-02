f4ec15baa0e0f64b1199137113a452443166350b
tjtanjin <cjtanjin@gmail.com>
2023-08-02 23:59:51 +0800

    fix: Fix eslint config and exports

--- src/export.tsx
+++ src/export.tsx
@@ -1,8 +1,8 @@
 import ChatBot from "./components/ChatBot";
 import { getDefaultBotOptions } from "./services/BotOptionsService";
-import { useBotOptions, BotOptionsContext } from "./context/BotOptionsContext";
-import { useMessages, MessagesContext } from "./context/MessagesContext";
-import { usePaths, PathsContext } from "./context/PathsContext";
+import { BotOptionsContext } from "./context/BotOptionsContext";
+import { MessagesContext } from "./context/MessagesContext";
+import { PathsContext } from "./context/PathsContext";
 import { Block } from "./types/Block";
 import { Flow } from "./types/Flow";
 import { Message } from "./types/Message";
