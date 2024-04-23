9a98e9420303b15050b06a6544a60679de4538d1
haecheonlee <dlgocjs1@gmail.com>
2024-04-23 19:39:43 -0400

    Fix space

--- vite.config.js
+++ vite.config.js
@@ -7,46 +7,45 @@ import eslint from "vite-plugin-eslint";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: "src",
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, "src/index.tsx"),
-            name: "react-chatbotify",
-            fileName: "index",
-            formats: ["es", "cjs"],
-        },
-        rollupOptions: {
-            external: ["react", "react-dom"],
-            output: {
-                globals: {
-                    react: "React",
-                },
-                // todo: deprecate and remove in 2.0.0, breaking change!
-                assetFileNames: (assetInfo) => {
-                    if (assetInfo.name === "style.css") return "react-chatbotify.css";
-                    return assetInfo.name;
-                },
-                intro: 'import "./react-chatbotify.css";',
-            },
-        },
-        outDir: "../dist",
+  root: "src",
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.tsx"),
+      name: "react-chatbotify",
+      fileName: "index",
+      formats: ["es", "cjs"],
     },
-    assetsInclude: ["**/*.svg", "**/*.png", "**/*.wav"],
-    plugins: [
-        svgr({
-            svgrOptions: {
-                ref: true,
-            },
-        }),
-        react({
-            include: "**/*.{jsx,tsx}",
-        }),
-        dts(),
-        eslint(),
-    ],
-    server: {
-        port: 3000,
-        host: true,
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+        // todo: deprecate and remove in 2.0.0, breaking change!
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "style.css") return "react-chatbotify.css";
+          return assetInfo.name;
+        },
+        intro: 'import "./react-chatbotify.css";',
+      },
     },
+    outDir: "../dist",
+  },
+  assetsInclude: ["**/*.svg", "**/*.png", "**/*.wav"],
+  plugins: [
+    svgr({
+      svgrOptions: {
+        ref: true,
+      },
+    }),
+    react({
+      include: "**/*.{jsx,tsx}",
+    }),
+    dts(),
+    eslint()
+  ],
+  server: {
+    port: 3000,
+    host: true,
+  },
 });
-
