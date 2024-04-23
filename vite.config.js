36d69a27e125c5028d34109d25f03f7743d291ac
haecheonlee <dlgocjs1@gmail.com>
2024-04-23 03:54:29 -0400

    feat:setup eslint locally

--- vite.config.js
+++ vite.config.js
@@ -2,49 +2,51 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import dts from "vite-plugin-dts";
 import path from "path";
+import eslint from "vite-plugin-eslint";
 
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src",
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.tsx"),
-      name: "react-chatbotify",
-      fileName: "index",
-      formats: ["es", "cjs"],
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
+    root: "src",
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/index.tsx"),
+            name: "react-chatbotify",
+            fileName: "index",
+            formats: ["es", "cjs"],
         },
-        // todo: deprecate and remove in 2.0.0, breaking change!
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === "style.css") return "react-chatbotify.css";
-          return assetInfo.name;
+        rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+                globals: {
+                    react: "React",
+                },
+                // todo: deprecate and remove in 2.0.0, breaking change!
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name === "style.css") return "react-chatbotify.css";
+                    return assetInfo.name;
+                },
+                intro: 'import "./react-chatbotify.css";',
+            },
         },
-        intro: 'import "./react-chatbotify.css";',
-      },
+        outDir: "../dist",
     },
-    outDir: "../dist",
-  },
-  assetsInclude: ["**/*.svg", "**/*.png", "**/*.wav"],
-  plugins: [
-    svgr({
-      svgrOptions: {
-        ref: true,
-      },
-    }),
-    react({
-      include: "**/*.{jsx,tsx}",
-    }),
-    dts()
-  ],
-  server: {
-    port: 3000,
-    host: true,
-  },
+    assetsInclude: ["**/*.svg", "**/*.png", "**/*.wav"],
+    plugins: [
+        svgr({
+            svgrOptions: {
+                ref: true,
+            },
+        }),
+        react({
+            include: "**/*.{jsx,tsx}",
+        }),
+        dts(),
+        eslint(),
+    ],
+    server: {
+        port: 3000,
+        host: true,
+    },
 });
 
