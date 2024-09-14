2e7f3ae1feb77f976a2eb7f4573fc0f54ad03841
Tan Jin <cjtanjin@gmail.com>
2024-09-14 18:44:32 +0800

    chore: Delete old text area change value event file

--- src/types/events/RcbTextareaChangeValue.ts
+++
@@ -1,9 +0,0 @@
-import { RcbBaseEvent } from "../internal/events/RcbBaseEvent";
-
-/**
- * Defines the data available for stop stream message event.
- */
-export type RcbTextAreaChangeValueEvent = RcbBaseEvent<{
-    currValue: string;
-    prevValue: string;
-}>;
