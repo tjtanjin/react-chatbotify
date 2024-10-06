2b506e43eb5faaf31f405222a342b34870f19d03
tjtanjin <cjtanjin@gmail.com>
2024-10-07 00:41:25 +0800

    test: Fix library test

--- cypress/fixtures/templates/ChatBotApp.tsx
+++ cypress/fixtures/templates/ChatBotApp.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
+import { useState } from "react";
 import ChatBot, { Flow, Params } from "react-chatbotify";
 
 function App() {
