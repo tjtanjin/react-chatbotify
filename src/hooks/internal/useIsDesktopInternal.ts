56fcc12a50d7139363931974618663f7e869449e
tjtanjin <cjtanjin@gmail.com>
2024-10-20 23:17:53 +0800

    refactor: Improve desktop and mobile devices check

--- src/hooks/internal/useIsDesktopInternal.ts
+++ src/hooks/internal/useIsDesktopInternal.ts
@@ -5,7 +5,12 @@ export const useIsDesktopInternal = () => {
 		if (typeof window === 'undefined' || !window.navigator) {
 			return false; // Default to false if running on server-side
 		}
-		return !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+		const userAgent = navigator.userAgent;
+    	const isNotMobileUA = !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent));
+    	const isWideEnough = window.innerWidth >= 768;
+
+    	// device is desktop if it is not a mobile agent and if the width is wide enough
+    	return isNotMobileUA && isWideEnough;
 	}, []);
 
 	// boolean indicating if user is on desktop (otherwise treated as on mobile)
