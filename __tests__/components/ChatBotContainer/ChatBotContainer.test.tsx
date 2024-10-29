5db5a3ec707bfe0965313b671c0b2155f1132cf1
George Olson <golson@pdx.edu>
2024-10-29 15:30:49 -0700

    Successfully got test passing for ChatBotContainer

--- __tests__/components/ChatBotContainer/ChatBotContainer.test.tsx
+++ __tests__/components/ChatBotContainer/ChatBotContainer.test.tsx
@@ -9,8 +9,8 @@ import { useBotStatesContext } from "../../../src/context/BotStatesContext";
 import { useSettingsContext } from "../../../src/context/SettingsContext";
 import { useStylesContext } from "../../../src/context/StylesContext";
 import { useBotRefsContext } from "../../../src/context/BotRefsContext";
+import { useNotificationInternal } from "../../../src/hooks/internal/useNotificationsInternal"; 
 
-// Mocking necessary hooks and context providers
 jest.mock("../../../src/hooks/internal/useButtonsInternal");
 jest.mock("../../../src/hooks/internal/useChatWindowInternal");
 jest.mock("../../../src/hooks/internal/useIsDesktopInternal");
@@ -18,6 +18,7 @@ jest.mock("../../../src/context/BotStatesContext");
 jest.mock("../../../src/context/SettingsContext");
 jest.mock("../../../src/context/StylesContext");
 jest.mock("../../../src/context/BotRefsContext");
+jest.mock("../../../src/hooks/internal/useNotificationsInternal"); 
 
 describe("ChatBotContainer Component", () => {
 	beforeEach(() => {
@@ -40,6 +41,10 @@ describe("ChatBotContainer Component", () => {
 		(useBotStatesContext as jest.Mock).mockReturnValue({
 			hasFlowStarted: false,
 			setHasFlowStarted: jest.fn(),
+			setUnreadCount: jest.fn(),
+			setTextAreaDisabled: jest.fn(), 
+			setIsChatWindowOpen: jest.fn(),
+			setAudioToggledOn: jest.fn(),
 		});
 
 		(useSettingsContext as jest.Mock).mockReturnValue({
@@ -62,18 +67,22 @@ describe("ChatBotContainer Component", () => {
 		});
 
 		(useBotRefsContext as jest.Mock).mockReturnValue({
-			flowRef: { current: {} },  // Mocked flowRef
+			flowRef: { current: {} },
 			chatBodyRef: { current: document.createElement('div') },
 			streamMessageMap: new Map(),
 			paramsInputRef: { current: null },
 			keepVoiceOnRef: { current: false },
+		});
+
+		(useNotificationInternal as jest.Mock).mockReturnValue({
+			audioContextRef: { current: {} },
+			setUpNotifications: jest.fn(),
 		});
 	});
 
 	it("renders ChatBotContainer with header, body, input, and footer", () => {
 		render(<ChatBotContainer />);
 
-		// Check if the header, body, input, and footer components are rendered
 		const headerButton = screen.getByText("Header Button");
 		const inputButton = screen.getByText("Input Button");
 		const footerButton = screen.getByText("Footer Button");
