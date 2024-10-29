bc11af7a1bbedc598d478a1877de3b3e4561f75e
tjtanjin <cjtanjin@gmail.com>
2024-10-30 00:48:48 +0800

    refactor: Allow individual styles for toasts

--- src/components/ChatBotBody/ToastPrompt/ToastPrompt.tsx
+++ src/components/ChatBotBody/ToastPrompt/ToastPrompt.tsx
@@ -29,6 +29,12 @@ const Toast = ({
 	// handles styles
 	const { styles } = useStylesContext();
 
+	// localized styles to prevent conflicting toast style updates
+	const [localizedStyles] = useState({
+		toastPromptStyle: { ...styles.toastPromptStyle },
+		toastPromptHoveredStyle: { ...styles.toastPromptHoveredStyle }
+	});
+
 	// handles toasts
 	const { dismissToast } = useToastsInternal();
 
@@ -39,7 +45,7 @@ const Toast = ({
 	const toastPromptHoveredStyle: React.CSSProperties = {
 		color: settings.general?.primaryColor,
 		borderColor: settings.general?.primaryColor,
-		...styles.toastPromptHoveredStyle
+		...localizedStyles.toastPromptHoveredStyle
 	};
 
 	useEffect(() => {
@@ -71,7 +77,7 @@ const Toast = ({
 			<div
 				onMouseEnter={handleMouseEnter}
 				onMouseLeave={handleMouseLeave} 
-				style={isHovered ? toastPromptHoveredStyle : styles.toastPromptStyle}
+				style={isHovered ? toastPromptHoveredStyle : localizedStyles.toastPromptStyle}
 				onMouseDown={(event: MouseEvent) => {
 					if (settings.toast?.dismissOnClick) {
 						event.preventDefault();
