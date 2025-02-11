6d720dfa1b2968142fed4ce5692ea6800e8c828b
tjtanjin <cjtanjin@gmail.com>
2025-02-12 02:19:12 +0800

    test: Fix cypress test cases

--- cypress/fixtures/templates/ChatBotApp.tsx
+++ cypress/fixtures/templates/ChatBotApp.tsx
@@ -123,10 +123,11 @@ function App() {
 		},
 		loop: {
 			message: (params: Params) => {
-				// sends the message half a second later to facilitate testing of new message prompt
+				// sends the message 5 seconds later to facilitate testing of new message prompt
+				// it is set to 5 seconds because running on hosted CI may be slow sometimes
 				setTimeout(async () => {
 					await params.injectMessage("You have reached the end of the conversation!");
-				}, 500)
+				}, 5000)
 			},
 			path: "loop"
 		},
